6cbb83b2d4e04f4190794fc4492fecda1998dfe0
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-06 22:01:35 +0530

    create store for users_state

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { usersReducer } from "./slices/UsersSlice";
+import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
 	reducer: {
@@ -8,3 +8,5 @@ export const store = configureStore({
 });
 
 export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
