1216a87d88df541b5d916320695fc7053fdb15ed
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-06 22:04:19 +0530

    added custom property

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				skeleton: {
+					400: `hsl(var(--bg-skeleton-400))`,
+					500: `hsl(var(--bg-skeleton-500))`,
+				},
+			},
+			backgroundSize: {
+				"x-dbl-y-full": "200% 100%",
+			},
+			keyframes: {
+				shimmer: {
+					"100%": {
+						backgroundPosition: "-150% 0%",
+					},
+				},
+			},
+			animation: {
+				shimmer: "shimmer 1.5s infinite",
+			},
+		},
 	},
 	plugins: [],
 };
