184c10766d145064d7927d488fb5cb2945c821b7
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-06 21:59:26 +0530

    created usersSlice

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,26 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
-import fetchUsers from "../thunks/fetchUsers";
+import { fetchUsers } from "../thunks/fetchUsers";
+import { addUser } from "../thunks/addUser";
+import { removeUser } from "../thunks/removeUser";
 
 const usersSlice = createSlice({
 	name: "users",
 	initialState: {
-		isLoading: false,
 		data: [],
-		error: null,
 	},
 	extraReducers(builder) {
-		builder.addCase(fetchUsers.pending, (state, action) => {
-			//update state however appropriately
-			// to show user that we are loading data from server
-			state.isLoading = true;
-		});
+		// builder.addCase(fetchUsers.pending, (state, action) => {
+		// 	//update state however appropriately
+		// 	// to show user that we are loading data from server
+		// 	state.isLoading = true;
+		// });
 		builder.addCase(fetchUsers.fulfilled, (state, action) => {
-			state.isLoading = false;
 			state.data = action.payload;
 		});
-		builder.addCase(fetchUsers.rejected, (state, action) => {
-			state.isLoading = false;
-			state.error = action.payload;
+		// builder.addCase(fetchUsers.rejected, (state, action) => {
+		// 	state.isLoading = false;
+		// 	state.error = action.payload;
+		// });
+		// builder.addCase(addUser.pending, (state, action) => {
+		// 	state.isLoading = true;
+		// });
+		builder.addCase(addUser.fulfilled, (state, action) => {
+			state.data.push(action.payload);
+		});
+		// builder.addCase(addUser.rejected, (state, action) => {
+		// 	state.isLoading = false;
+		// 	state.error = action.payload;
+		// });
+		builder.addCase(removeUser.fulfilled, (state, action) => {
+			state.data = state.data.filter((user) => user.id !== action.payload.id);
 		});
 	},
 });
