26d68c8b647bff63732b0aca97aaeb2643ee3664
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-06 22:00:21 +0530

    created Async Thunk

---
+++ src/store/thunks/addUser.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { faker } from "@faker-js/faker";
+
+export const addUser = createAsyncThunk("users/add", async () => {
+	const res = await axios.post("http://localhost:3005/users", {
+		name: faker.person.fullName(),
+	});
+	return res.data;
+});
