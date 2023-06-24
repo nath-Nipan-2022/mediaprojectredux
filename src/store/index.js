14517e3ff17335fb4cd2c6b2327cf76814ee9db7
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-24 09:34:03 +0530

    updated store for albumApi queries

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
+		[albumsApi.reducerPath]: albumsApi.reducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(albumsApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
