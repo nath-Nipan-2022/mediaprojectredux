a767dbe59d822b7375a9e226c5b5cd109fa96049
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-24 19:48:35 +0530

    ui modified

--- src/components/ExpandablePanel.jsx
+++ src/components/ExpandablePanel.jsx
@@ -6,16 +6,21 @@ function ExpandablePanel({ header, children, className }) {
 	const [isExpanded, setIsExpanded] = useState(false);
 
 	return (
-		<div className={`mb-4 rounded ${className}`}>
-			<div className="px-2 flex justify-between items-center border border-gray-300 rounded-sm shadow-sm">
-				<h3 className="p-3 flex justify-between items-center">{header}</h3>
-
+		<div className={`mb-4 ${className}`}>
+			<div
+				className={`px-2 flex justify-between items-center border border-gray-300 shadow ${
+					isExpanded ? "bg-blue-50 rounded-t" : "rounded"
+				}`}
+			>
+				<h3 className="p-3 flex font-medium justify-between items-center">
+					{header}
+				</h3>
 				<Button
 					rounded
 					className="text-gray-500 hover:bg-blue-200"
 					onClick={() => setIsExpanded(!isExpanded)}
 				>
-					{isExpanded ? <GoChevronDown /> : <GoChevronUp />}
+					{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
 				</Button>
 			</div>
 			{isExpanded && children}
