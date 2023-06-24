b6f719cf7a3775b98e600731d038907bd8257c25
Nipan Debnath <debnathnipan719@gmail.com>
2023-06-24 19:41:12 +0530

    rendered photosList  in AlbumList

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import { GoX } from "react-icons/go";
-import ExpandablePanel from "./ExpandablePanel";
 import AlbumListItem from "./AlbumListItem";
 
 import {
@@ -11,12 +10,11 @@ import {
 } from "../store/apis/albumsApi";
 
 function AlbumList({ user }) {
-	const { data, isLoading, error } = useFetchAlbumsQuery(user);
-	const [addAlbum, { isLoading: isAddingAlbum, error: albumError }] =
-		useAddAlbumMutation();
+	const { data, isFetching, error } = useFetchAlbumsQuery(user);
+	const [addAlbum, results] = useAddAlbumMutation();
 
 	let content = "";
-	if (isLoading) {
+	if (isFetching) {
 		content = <Skeleton times={3} className={"h-10 w-full rounded"} />;
 	} else if (error) {
 		content = "Error loading album list";
@@ -27,17 +25,17 @@ function AlbumList({ user }) {
 	}
 
 	return (
-		<div className="p-4 album border border-blue-200">
+		<div className="p-4 bg-[#98989824] border border-blue-200">
 			<div className="flex justify-between mb-3">
 				<h3 className="font-medium mb-4">Albums for {user.name}</h3>
 				<Button
-					loading={isAddingAlbum}
+					loading={results.isLoading}
 					onClick={() => addAlbum(user)}
 					className="bg-indigo-500 text-white hover:bg-indigo-600"
 				>
 					Add Album
 				</Button>
-				{albumError && "Error adding album"}
+				{results.error && "Error adding album"}
 			</div>
 
 			{content}
